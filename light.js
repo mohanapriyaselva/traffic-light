374bdde169aa1ffc1cef22995e2ded6d288ce554
mohanapriyaselva <143673133+mohanapriyaselva@users.noreply.github.com>
2023-10-27 19:25:56 +0530

    Add files via upload

---
+++ light.js
@@ -0,0 +1,6 @@
+function bulb_on(){
+    document.getElementById("cat").src="https://i.pinimg.com/originals/0b/56/98/0b5698b87812234d213575ddc52235c5.jpg";
+        }
+function bulb_off(){
+    document.getElementById("cat").src="https://wallpaperaccess.com/full/1158162.jpg";
+}
